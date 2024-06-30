dfeb5c5748a329f3ac87fc60cd58a84bf421e121
vagu21 <bharadwaj21197@gmail.com>
2024-06-30 22:29:40 +0530

    crud completed

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-const express=require('express')
-const mongoose=require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
+const Task = require('./model'); // Import the model, not the schema
+const cors=require('cors')
 
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: '*'
+})) 
 
-const app=express()
-mongoose.connect('mongodb+srv://user:user@cluster0.pfionep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(
-    ()=>console.log("DB Connected")
-)
+mongoose.connect('mongodb+srv://user:user@cluster0.pfionep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err)); 
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+app.post('/addtask', async (req, res) => {
+  const { todo } = req.body;
+  try {
+    const newData = new Task({ todo });
+    await newData.save();
+    return res.json(await Task.find()); // Use Task instead of TaskSchema
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Error adding task' }); // Return an error response
+  }
+});
+app.get('/gettask', async(req, res) => {
+    try{
+return res.json(await Task.find())
+    }catch(err){
+console.log(err);
+    }
+  });
+  
+app.delete('/delete/:id',async(req,res)=>{
+    try{
+await Task.findByIdAndDelete(req.params.id)
+return res.json(await Task.find())
+    }catch(err){
+       console.log(err)
+    }
 })
-app.listen(5000,()=>{
-    console.log('server is running')
-})
+app.listen(5000, () => {
+  console.log('server is running')
+});
